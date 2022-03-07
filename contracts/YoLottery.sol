017939cf274c7630a49317fe1665ab950d575e31
Matthew <mtwichan@gmail.com>
2022-03-06 19:30:40 -0800

    feat: convert pool variables to struct

--- contracts/YoLottery.sol
+++ contracts/YoLottery.sol
@@ -8,21 +8,22 @@ import "hardhat/console.sol";
     2. Transfer money in and out of pool -> Complete
     3. Randomize distribution of funds -> WIP -- need to wait for Chainlink
     4. Unlock pool after some time interval -> WIP
-    5. Change types to optimal values, ex: all uints should not be uint256 -> WIP
-    6. Add modifiers -> WIP
+    5. Change types to optimal values -> WIP
+    6. Add modifiers -> Complete
     7. Start pool 
-    - minimum buy in and minimum total pool?
-    - reward for putting more funds in? - get larger chunk of left over funds?
-    8. Starting pool parameters -> WIP
-    4. How to figure out the time intervals
-    - when does the pool start
-    - would need to do -> time_now - time_pool_started < time_interval
+    - minimum buy in minimum total pool -> Complete
+    8. Starting pool parameters -> Complete
+    9. Add ticket system so fixed cost bets to get a ticket - each ticket gives a play in probability game
+    10. Support ERC20 token (USDT) -> will need to write another contract
+    11. Time release pool by letting user free it for small reward -> WIP
 */
 contract YoLottery {
+    
     /* Modifiers */
+
     // Ensure the address calling function is the owner
     modifier onlyOwner() {
-        require(msg.sender == owner);
+        require(msg.sender == owner, "Only the owner of the contract can access this function");
         _;
     }
 
@@ -34,40 +35,42 @@ contract YoLottery {
 
     // Ensure the pool is not currently running
     modifier poolUnlocked (uint256 poolNumber) {
-        require((block.timestamp - poolTime[poolNumber].startTime) >  poolTime[poolNumber].interval);
+        require((block.timestamp - pool[poolNumber].startTime) >  pool[poolNumber].interval, "The pool is unlocked");
         _;
     }
 
     // Ensure the pool is currently running
     modifier poolLocked (uint256 poolNumber) {
-        require((block.timestamp - poolTime[poolNumber].startTime) <  poolTime[poolNumber].interval);
+        require((block.timestamp - pool[poolNumber].startTime) <  pool[poolNumber].interval, "The pool is locked");
         _;
     }
 
     // Ensure the pool exists
     modifier poolExists (uint256 poolNumber) {
-        require(pools[poolNumber] == true);
+        require(activePools[poolNumber] == true, "The pool already exists");
         _;
     }
 
     // Ensure the pool does not exists
     modifier poolDoesNotExist (uint256 poolNumber) {
-        require(pools[poolNumber] == false);
+        require(activePools[poolNumber] == false, "The pool does not exist");
         _;
     }
 
     /* Variables */
-    struct PoolTiming{
-        uint startTime;
-        uint interval;
-        bool locked;
+    struct Pool{
+        uint256 startTime;
+        uint256 interval;
+        uint256 balance;
+        uint256 minBuyIn;
+        address[] participants;
     }
     mapping(address => mapping(uint256 => uint256)) public balance; // I don't think I need this because we don't need to track how much a user put in the pool really
     mapping(address => uint256) public owedAmount; 
-    mapping(uint256 => address[]) public participants;
-    mapping(uint256 => bool) pools;
-    mapping(uint256 => uint256) poolBalance;
-    mapping(uint256 => PoolTiming) poolTime;
+    // mapping(uint256 => mapping(address => bool)) participants;
+    mapping(address => mapping(uint256 => bool)) public participants;
+    mapping(uint256 => bool) activePools;
+    mapping(uint256 => Pool) pool;
     
     address owner;
     
@@ -80,66 +83,56 @@ contract YoLottery {
     }
 
     // Allow owner to create and initialize a pool
-    function createPool(uint256 poolNumber, uint256 poolTimeInterval) external onlyOwner poolDoesNotExist(poolNumber) {
-        pools[poolNumber] = true;
-        poolTime[poolNumber].startTime = block.timestamp;
-        poolTime[poolNumber].interval = poolTimeInterval;
-        poolBalance[poolNumber] = 0;
+    function createPool(uint256 poolNumber, uint256 poolTimeInterval, uint256 poolBuyIn) external onlyOwner poolDoesNotExist(poolNumber) {
+        activePools[poolNumber] = true;
+        pool[poolNumber].interval = poolTimeInterval;
+        pool[poolNumber].minBuyIn = poolBuyIn;
     }
+
     // Allow owner to remove pool
     function removePool(uint256 poolNumber) external onlyOwner poolExists(poolNumber) poolUnlocked(poolNumber) {
-        delete pools[poolNumber];
-        delete poolBalance[poolNumber];
-        delete poolTime[poolNumber];
+        delete activePools[poolNumber];
+        delete pool[poolNumber];
     }
 
-    // Start pool only if minimum buy in reached
-    function startPool(uint256 poolNumber) public poolExists(poolNumber) {
-        poolTime[poolNumber].startTime = block.timestamp;
-    }
-
-    // Allow owner to change time interval if pool is not running
-    function setPoolTimeInterval(uint256 poolNumber, uint256 poolTimeInterval) external onlyOwner poolUnlocked(poolNumber) {
-        poolTime[poolNumber].interval = poolTimeInterval; 
+    // Allow owner to start a pool
+    function startPool(uint256 poolNumber) public onlyOwner poolExists(poolNumber) {
+        pool[poolNumber].startTime = block.timestamp;
     }
 
     // User deposits ETH into pool
     function depositPool(uint256 poolNumber) payable external validateAddress poolExists(poolNumber) poolLocked(poolNumber) {
-        require(msg.sender.balance >= msg.value, "Ensure sender has enough funds to put in pool");
-        address[] storage _participants;
-        
+        require(msg.value >= pool[poolNumber].minBuyIn, "Deposited ETH must be greater or equal to the buy in");
+
         if (balance[msg.sender][poolNumber] > 0) {
             balance[msg.sender][poolNumber] = balance[msg.sender][poolNumber] + msg.value;
         } else {
             balance[msg.sender][poolNumber] = msg.value;
+            pool[poolNumber].participants.push(msg.sender);
         }
-        
-        _participants = participants[poolNumber];
-        _participants.push(msg.sender);
-
-        participants[poolNumber] = _participants;
-        
-        poolBalance[poolNumber] = poolBalance[poolNumber] + msg.value; 
+        pool[poolNumber].balance = pool[poolNumber].balance + msg.value;
     }
 
     // Distribute the ETH in the pool to the participants
     function distributePool(uint256 poolNumber) external poolExists(poolNumber) poolUnlocked(poolNumber) {
-        
-        address[] memory _participants = participants[poolNumber];
+        address[] memory _participants = pool[poolNumber].participants;
         address distributerAddress;
+        uint256 poolBalance = pool[poolNumber].balance;
+        
         for (uint256 idx = 0; idx < _participants.length; idx++) {
             distributerAddress = _participants[idx]; 
             owedAmount[distributerAddress] = owedAmount[distributerAddress] + 1 ether; // Random number
             balance[distributerAddress][poolNumber] = 0;
         }
-        poolBalance[poolNumber] = 0;
+        pool[poolNumber].balance = 0;
+        pool[poolNumber].participants = new address[](0);
     }
 
     // User withdraws funds owed to them
     function withdraw() payable external {
         require(owedAmount[msg.sender] > 0, "User must have funds oweing to them"); // ensure user can only pull out their funds
         uint256 owedFunds = owedAmount[msg.sender];
-        payable(msg.sender).transfer(owedFunds);
+        payable(msg.sender).transfer(owedFunds); // Look at best practices -- this may not be correct
         owedAmount[msg.sender] = 0;
         emit Withdrawl(msg.sender, owedFunds);
     }
@@ -148,6 +141,11 @@ contract YoLottery {
     // Deposit ETH into smart contract -- don't think this is correct
     function deposit(uint256 amount) payable external {
         require(msg.value == amount);
+    }
+
+    // Allow owner to set  time interval if pool is not running
+    function setPoolTimeInterval(uint256 poolNumber, uint256 poolTimeInterval) external onlyOwner poolUnlocked(poolNumber) {
+        pool[poolNumber].interval = poolTimeInterval; 
     }
 
     // Get the amount of ETH stored in the contract
@@ -159,6 +157,16 @@ contract YoLottery {
     function getBalance(uint256 poolNumber) external view returns (uint256) {
         return balance[msg.sender][poolNumber];
     }  
+
+    // Get the amount of time left until the specified pool unlocks
+    function getPoolUnlockTime(uint256 poolNumber) external view returns (uint256) {
+        return block.timestamp - pool[poolNumber].startTime;
+    }
+
+    // Get all metadata pertaining to a specific pool instance
+    function getPoolData(uint256 poolNumber) external view returns (Pool memory) {
+        return pool[poolNumber];
+    }
 
     // Get the amount owed to the user after the pool distribution
     function getOwedAmount() external view returns (uint256) {
